2525f6790d93a6d9a1152d2dc7db668e5943fb7b
4KV6 <dr.clever489@gmail.com>
2024-02-28 16:30:36 +0700

    make google login

---
+++ flask/app/models/authuser.py
@@ -0,0 +1,25 @@
+from flask_login import UserMixin
+from sqlalchemy_serializer import SerializerMixin
+
+from app import db
+
+class AuthUser(db.Model, UserMixin):
+    __tablename__ = "auth_users"
+    # primary keys are required by SQLAlchemy
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    name = db.Column(db.String(1000))
+    password = db.Column(db.String(100))
+    avatar_url = db.Column(db.String(100))
+
+    def __init__(self, email, name, password, avatar_url):
+        self.email = email
+        self.name = name
+        self.password = password
+        self.avatar_url = avatar_url
+
+    def update(self, email, name, password, avatar_url):
+        self.email = email
+        self.name = name
+        self.password = password
+        self.avatar_url = avatar_url
--- flask/app/views.py
+++ flask/app/views.py
@@ -1,3 +1,6 @@
+import secrets
+import string
+
 from flask import (jsonify, render_template,
                    request, url_for, flash, redirect)
 
@@ -6,9 +9,13 @@ from werkzeug.urls import url_parse
 
 from sqlalchemy.sql import text
 from flask_login import login_user, login_required, logout_user, current_user
+from markupsafe import escape
 
 from app import app
 from app import db
+from app import login_manager
+from app.models.authuser import AuthUser
+from app import oauth
 
 
 @app.route('/', methods=('GET', 'POST'))
@@ -22,8 +29,83 @@ def sign_up():
 
 
 @app.route('/play')
+@login_required
 def play():
-    return app.send_static_file("play.html")
+    return render_template("play.html")
+
+
+@app.route('/google/')
+def google():
+
+    oauth.register(
+        name='google',
+        client_id=app.config['GOOGLE_CLIENT_ID'],
+        client_secret=app.config['GOOGLE_CLIENT_SECRET'],
+        server_metadata_url=app.config['GOOGLE_DISCOVERY_URL'],
+        client_kwargs={
+            'scope': 'openid email profile'
+        }
+    )
+
+   # Redirect to google_auth function
+    redirect_uri = url_for('google_auth', _external=True)
+    return oauth.google.authorize_redirect(redirect_uri)
+
+
+@app.route('/google/auth/')
+def google_auth():
+    token = oauth.google.authorize_access_token()
+    app.logger.debug(str(token))
+    userinfo = token['userinfo']
+    app.logger.debug(" Google User " + str(userinfo))
+    email = userinfo['email']
+    user = AuthUser.query.filter_by(email=email).first()
+
+    if not user:
+        name = userinfo.get('given_name','') + " " + userinfo.get('family_name','')
+        random_pass_len = 8
+        password = ''.join(secrets.choice(string.ascii_uppercase + string.digits)
+                          for i in range(random_pass_len))
+        picture = userinfo['picture']
+        new_user = AuthUser(email=email, name=name,
+                           password=generate_password_hash(
+                               password, method='sha256'),
+                           avatar_url=picture)
+        db.session.add(new_user)
+        db.session.commit()
+        user = AuthUser.query.filter_by(email=email).first()
+    login_user(user)
+    return redirect('/play')
+
+
+def gen_avatar_url(email, name):
+    bgcolor = generate_password_hash(email, method='sha256')[-6:]
+    color = hex(int('0xffffff', 0) -
+                int('0x'+bgcolor, 0)).replace('0x', '')
+    lname = ''
+    temp = name.split()
+    fname = temp[0][0]
+    if len(temp) > 1:
+        lname = temp[1][0]
+
+    avatar_url = "https://ui-avatars.com/api/?name=" + \
+        fname + "+" + lname + "&background=" + \
+        bgcolor + "&color=" + color
+    return avatar_url
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our
+    # user table, use it in the query for the user
+    return AuthUser.query.get(int(user_id))
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/crash')
