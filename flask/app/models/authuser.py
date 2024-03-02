3567a5783c6a45579a900ff10795c7cb1aac27e5
4KV6 <dr.clever489@gmail.com>
2024-03-02 16:03:37 +0700

    database created (not test yet)

--- flask/app/models/authuser.py
+++ flask/app/models/authuser.py
@@ -2,6 +2,7 @@ from flask_login import UserMixin
 from sqlalchemy_serializer import SerializerMixin
 
 from app import db
+from app.models.quiz import Quiz
 
 class AuthUser(db.Model, UserMixin):
     __tablename__ = "auth_users"
@@ -11,6 +12,7 @@ class AuthUser(db.Model, UserMixin):
     name = db.Column(db.String(1000))
     password = db.Column(db.String(100))
     avatar_url = db.Column(db.String(250))
+    my_quizs = db.relationship('Quiz', backref='auth_users', lazy=True)
 
     def __init__(self, email, name, password, avatar_url):
         self.email = email
@@ -23,3 +25,11 @@ class AuthUser(db.Model, UserMixin):
         self.name = name
         self.password = password
         self.avatar_url = avatar_url
+
+
+class PrivateQuiz(Quiz, UserMixin, SerializerMixin):
+    created_by_id = db.Column(db.Integer, db.ForeignKey('auth_users.id'))
+
+    def __init__(self, quiz_name, is_time_limit, tag, difficulty, own_id):
+        super().__init__(quiz_name, is_time_limit, tag, difficulty)
+        self.created_by_id = own_id
